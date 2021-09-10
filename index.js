364e6ab20dae2c21732aa41aa5656727c114b7c2
legalvao1 <legalvao1@gmail.com>
2021-09-10 15:27:55 -0300

    Criando a pasta de rotas e inicializando o express

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT;
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(port, () => { console.log(`Aplicação ouvindo na porta ${port}`); });
