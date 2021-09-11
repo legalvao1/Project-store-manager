8057e26967ae35013dfcd3a66673aa5da3b91f95
legalvao1 <legalvao1@gmail.com>
2021-09-10 21:11:52 -0300

    Configurando o model para listar os todos os produtos e os produtos pelo id

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,4 +1,18 @@
+const { ObjectId } = require('mongodb');
 const mongoConnection = require('./connection');
+
+const getAllProducts = async () => {
+  const db = await mongoConnection.getConnection();
+  const productsCollection = await db.collection('products').find().toArray();
+  return productsCollection;
+};
+
+const findProductById = async (id) => {
+  if (!ObjectId.isValid(id)) return false;
+  const db = await mongoConnection.getConnection();
+  const product = await db.collection('products').findOne({ _id: ObjectId(id) });
+  return product;
+};
 
 const findProductByName = async (name) => {
   const db = await mongoConnection.getConnection();
@@ -18,6 +32,8 @@ const create = async (name, quantity) => {
 };
 
 module.exports = {
-    findProductByName,
-    create,
+  getAllProducts,
+  findProductById,
+  findProductByName,
+  create,
 };
