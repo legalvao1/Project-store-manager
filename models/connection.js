26d4c3cb72994a2246fc4017ae4371b885ed11da
legalvao1 <legalvao1@gmail.com>
2021-09-10 21:33:24 -0300

    Corrigindo nome da url no arquivo de connection

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-const { MONGO_DB_URL } = process.env || 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
 let schema = null;
