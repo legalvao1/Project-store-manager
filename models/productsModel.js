a8b984e8eeec4d1142293f9f6131d3483e2dbbed
legalvao1 <legalvao1@gmail.com>
2021-09-11 16:45:23 -0300

    desafio 3 - Configurando model do  updateProduct

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,28 +1,28 @@
 const { ObjectId } = require('mongodb');
-const mongoConnection = require('./connection');
+const { getConnection } = require('./connection');
 
 const getAllProducts = async () => {
-  const db = await mongoConnection.getConnection();
+  const db = await getConnection();
   const productsCollection = await db.collection('products').find().toArray();
   return productsCollection;
 };
 
 const findProductById = async (id) => {
   if (!ObjectId.isValid(id)) return false;
-  const db = await mongoConnection.getConnection();
+  const db = await getConnection();
   const product = await db.collection('products').findOne({ _id: ObjectId(id) });
   return product;
 };
 
 const findProductByName = async (name) => {
-  const db = await mongoConnection.getConnection();
+  const db = await getConnection();
   const product = await db.collection('products').findOne({ name });
   
   return product !== null;
 };
 
 const create = async (name, quantity) => {
-  const productsCollection = await mongoConnection.getConnection()
+  const productsCollection = await getConnection()
     .then((db) => db.collection('products'));
 
     const { insertedId: _id } = await productsCollection
@@ -31,9 +31,20 @@ const create = async (name, quantity) => {
   return { _id, name, quantity };
 };
 
+const update = async (id, name, quantity) => {
+  if (!ObjectId.isValid(id)) return false;
+
+  const db = await getConnection();
+  await db.collection('products').updateOne(
+    { _id: ObjectId(id) }, { $set: { name, quantity } },
+    );
+  return { id, name, quantity };
+};
+
 module.exports = {
   getAllProducts,
   findProductById,
   findProductByName,
   create,
+  update,
 };
