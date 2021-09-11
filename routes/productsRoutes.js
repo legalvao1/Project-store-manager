75bf50441425892f9b977432506b423d8c30a0ed
legalvao1 <legalvao1@gmail.com>
2021-09-10 21:10:33 -0300

    adicionando a rota para listar todos os produtos e os produtos pelo id

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 
 const productController = require('../controllers/productsController');
 
-router.get('/', () => {});
-router.get('/:id', () => {});
-router.post('/', productController.addProduct);
+router.get('/', productController.getAllProducts);
+router.get('/:id', productController.findProductById);
+router.post('/', productController.addProduct); 
 router.put('/:id', () => {});
 router.delete('/:id', () => {});
 
