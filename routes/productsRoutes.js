2a5d69b6146ed14132e1ce29607b42f074c087a7
legalvao1 <legalvao1@gmail.com>
2021-09-11 15:33:41 -0300

    Desafio 3 - rota para atualizar um produto

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -5,7 +5,7 @@ const productController = require('../controllers/productsController');
 router.get('/', productController.getAllProducts);
 router.get('/:id', productController.findProductById);
 router.post('/', productController.addProduct); 
-router.put('/:id', () => {});
+router.put('/:id', () => productController.updateProduct);
 router.delete('/:id', () => {});
 
 module.exports = router;
