9d910e8e6eb5f33859001a196cca3cede8d69adb
legalvao1 <legalvao1@gmail.com>
2021-09-10 21:11:14 -0300

    Configurando o controller para listar os todos os produtos e os produtos pelo id

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,4 +1,18 @@
 const productsService = require('../services/productsService');
+const productsModel = require('../models/productsModel');
+
+const getAllProducts = async (_req, res) => {
+    const products = await productsModel.getAllProducts();
+    res.status(200).json({ products });
+};
+
+const findProductById = async (req, res) => {
+  const { id } = req.params; 
+  const product = await productsService.findProductById(id);
+
+  if (product.err) return res.status(422).json(product);
+  res.status(200).json(product);
+};
 
 const addProduct = async (req, res) => {
   const { name, quantity } = req.body;
@@ -11,5 +25,7 @@ const addProduct = async (req, res) => {
 };
 
 module.exports = {
+  getAllProducts,
+  findProductById,
   addProduct,
 };
