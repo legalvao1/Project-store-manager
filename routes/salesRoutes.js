67ef93d3bc141af6d5274fb417dbe9eba5a18341
legalvao1 <legalvao1@gmail.com>
2021-09-13 14:29:47 -0300

    Desafio 8 - Rota para deletar uma venda

--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -6,6 +6,6 @@ router.get('/', salesController.getAllSales);
 router.get('/:id', salesController.getSaleById);
 router.post('/', salesController.addSale);
 router.put('/:id', salesController.updateSale);
-router.delete('/:id', () => {});
+router.delete('/:id', salesController.deleteSale);
 
 module.exports = router;
