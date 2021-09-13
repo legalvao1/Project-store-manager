d1a01b37927012fc178c69bd6b97956b4e87c9a4
legalvao1 <legalvao1@gmail.com>
2021-09-13 13:57:25 -0300

    Desafio 7 - Controller para atualizar uma venda

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -22,8 +22,18 @@ const addSale = async (req, res) => {
   res.status(200).json(sale);
 };
 
+const updateSale = async (req, res) => {
+  const { id } = req.params;
+  const sale = await salesService.updateSale(id, req.body);
+
+  if (sale.err) return res.status(422).json(sale);
+
+  res.status(200).json(sale);
+};
+
 module.exports = {
   getAllSales,
   getSaleById,
   addSale,
+  updateSale,
 };
