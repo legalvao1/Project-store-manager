e540e39667a3e7309b9771224196d04211554e05
legalvao1 <legalvao1@gmail.com>
2021-09-14 16:15:28 -0300

    Desafio 11 - testes para productsModel

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,28 +1,28 @@
 const { ObjectId } = require('mongodb');
-const { getConnection } = require('./connection');
+const mongoConnection = require('./connection');
 
 const getAllProducts = async () => {
-  const db = await getConnection();
+  const db = await mongoConnection.getConnection();
   const productsCollection = await db.collection('products').find().toArray();
   return productsCollection;
 };
 
 const findProductById = async (id) => {
   if (!ObjectId.isValid(id)) return false;
-  const db = await getConnection();
+  const db = await mongoConnection.getConnection();
   const product = await db.collection('products').findOne({ _id: ObjectId(id) });
   return product;
 };
 
 const findProductByName = async (name) => {
-  const db = await getConnection();
+  const db = await mongoConnection.getConnection();
   const product = await db.collection('products').findOne({ name });
   
   return product !== null;
 };
 
 const create = async (name, quantity) => {
-  const productsCollection = await getConnection()
+  const productsCollection = await mongoConnection.getConnection()
     .then((db) => db.collection('products'));
 
     const { insertedId: _id } = await productsCollection
@@ -34,7 +34,7 @@ const create = async (name, quantity) => {
 const update = async (id, name, quantity) => {
   if (!ObjectId.isValid(id)) return false;
 
-  const db = await getConnection();
+  const db = await mongoConnection.getConnection();
   await db.collection('products').updateOne(
     { _id: ObjectId(id) }, { $set: { name, quantity } },
     );
@@ -44,7 +44,7 @@ const update = async (id, name, quantity) => {
 const exclude = async (id) => {
   if (!ObjectId.isValid(id)) return false;
 
-  const db = await getConnection();
+  const db = await mongoConnection.getConnection();
   const product = await db.collection('products').deleteOne({ _id: ObjectId(id) });
   return product;
 };
--- test/unit/models.js
+++ test/unit/models.js
@@ -2,31 +2,36 @@ const sinon = require('sinon');
 const { expect } = require('chai');
 const { MongoClient, ObjectId } = require('mongodb');
 const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongoConnection = require('../../models/connection');
 
 const productsModel = require('../../models/productsModel');
-const salesModel = require('../../models/salesModel');
 
-describe('Busca todos os produtos no BD', () => {
-  describe('quando não existe nenhum produto cadastrado', async () => {
-    const DBServer = new MongoMemoryServer();
 
-    before(async () => {
-      const URLMock = await DBServer.getUri();
-      const connectionMock = await MongoClient
-        .connect(URLMock, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true
-        });
+describe('Busca todos os Produtos no BD', () => {
+  const DBServer = new MongoMemoryServer();
+  let connectionMock;
 
-      sinon.stub(MongoClient, 'connect')
-        .resolves(connectionMock);
-    });
+  before(async () => {
+    const URLMock = await DBServer.getUri();
+    connectionMock = await MongoClient
+      .connect(URLMock, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+      .then((conn) => conn.db('model_tests'))
+      
 
-    after(async () => {
-      MongoClient.connect.restore();
-      await DBServer.stop();
-    });
+    sinon.stub(mongoConnection, 'getConnection')
+      .resolves(connectionMock);
+  });
 
+  after(async () => {
+    mongoConnection.getConnection.restore();
+    await DBServer.stop();
+  });
+
+
+  describe('quando não existe nenhum produto cadastrado', () => {
     it('retorna um array', async () => {
       const response = await productsModel.getAllProducts();
 
@@ -34,7 +39,7 @@ describe('Busca todos os produtos no BD', () => {
     });
 
     it('o array está vazio', async () => {
-      const response = await productsModel.getAllProducts();;
+      const response = await productsModel.getAllProducts();
 
       expect(response).to.be.empty;
     });
@@ -42,55 +47,326 @@ describe('Busca todos os produtos no BD', () => {
   });
 
   describe('quando existem produtos cadastrados', () => {
-    const DBServer = new MongoMemoryServer();
+    const productExample = { name: 'example', quantity: 20};
 
     before(async () => {
-      const URLMock = await DBServer.getUri();
-      const connectionMock = await MongoClient
-        .connect(URLMock, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true
-        });
-
-      sinon.stub(MongoClient, 'connect')
-        .resolves(connectionMock);
-
-      const productsCollection = await connectionMock.db('StoreManager').collection('products');
-
-      await productsCollection.insertOne({
-        name: 'pudim',
-        quantity: 20,
-      })
+      const db = await mongoConnection.getConnection();
+      await db.collection('products').insertOne({ ...productExample });
     });
 
     after(async () => {
-      MongoClient.connect.restore();
-      await DBServer.stop();
-    });
+      const db = await mongoConnection.getConnection();
+      await db.collection('products').drop();
+  });
+
 
     it('retorna um array', async () => {
-      const response = await productsModel.getAllProducts();;
+      const response = await productsModel.getAllProducts();
 
       expect(response).to.be.an('array');
     });
 
     it('o array não está vazio', async () => {
-      const response = await productsModel.getAllProducts();;
+      const response = await productsModel.getAllProducts();
 
       expect(response).to.be.not.empty;
     });
 
     it('o array possui itens do tipo objeto', async () => {
-      const [ item ] = await productsModel.getAllProducts();;
+      const [ item ] = await productsModel.getAllProducts();
 
       expect(item).to.be.an('object');
     });
 
-    it('tais itens possui as propriedades: "id", "name", "quantity"', async () => {
-      const [ item ] = await productsModel.getAllProducts();;
+    it('tais itens possui as propriedades: "_id", "name", "quantity"', async () => {
+      const [ item ] = await productsModel.getAllProducts();
 
       expect(item).to.include.all.keys('_id', 'name', 'quantity')
     });
+  });
+});
 
+describe('Cadastra um novo produto no banco de dados', () => {
+  const DBServer = new MongoMemoryServer();
+  let connectionMock
+
+  const productExample = {
+    // _id: '604cb554311d68f491ba5781',
+    name: 'example',
+    quantity: 20,
+  };
+
+  before(async () => {
+    const URLMock = await DBServer.getUri();
+    connectionMock = await MongoClient
+      .connect(URLMock, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+      .then((conn) => conn.db('StoreManager'));
+
+    sinon.stub(mongoConnection, 'getConnection')
+      .resolves(connectionMock);
+  });
+
+  after(async () => {
+    mongoConnection.getConnection.restore();
+    await DBServer.stop();
+  });
+
+  describe('Quando um produto é cadastrado com sucesso', () =>{
+    it('retorna um objeto', async () => {
+      const response = await productsModel.create(productExample);
+      expect(response).to.be.a('object');
+    });
+
+    it('o objeto possui as propriedades _id, name, quantity', async () => {
+      const response = await productsModel.create(productExample);
+      expect(response).to.include.all.keys('_id', 'name', 'quantity')
+    })
+  });
+});
+
+describe('Busca apenas um filme no BD através do ID', () => {
+  const DBServer = new MongoMemoryServer();
+  let connectionMock;
+
+  const ID_EXAMPLE = '604cb554311d68f491ba5781';
+
+  before(async () => {
+    const URLMock = await DBServer.getUri();
+    connectionMock = await MongoClient
+     .connect(URLMock, {
+       useNewUrlParser: true,
+       useUnifiedTopology: true
+     })
+     .then((conn) => conn.db('StoreManager'));
+
+    
+    sinon.stub(mongoConnection, 'getConnection').resolves(connectionMock);
+  });
+
+  after(() => {
+    mongoConnection.getConnection.restore();
+  });
+
+  describe('quando não existe um filme para o ID informado', () => {
+    it('retorna "null"', async () => {
+      const response = await productsModel.findProductById(ID_EXAMPLE);
+
+      expect(response).to.be.equal(null);
+    });
+  });
+
+  describe('quando existe um filme para o ID informado', () => {
+    const expectedProduct = {
+      _id: ObjectId('604cb554311d68f491ba5781'),
+      name: 'example',
+      quantity: 20,
+    }
+
+    before(async () => {
+      const db = await mongoConnection.getConnection();
+      await db.collection('products').insertOne({ ... expectedProduct });
+    });
+
+    after(async () => {
+      const db = await mongoConnection.getConnection();
+      await db.collection('products').drop();
+  });
+
+    it('retorna um objeto', async () => {
+      const response = await productsModel.findProductById(ID_EXAMPLE);
+      console.log(response)
+
+      expect(response).to.be.a('object');
+    });
+
+    it('o objeto possui as propriedades: "_id", "name", "quantity"', async () => {
+      const response = await await productsModel.findProductById(ID_EXAMPLE);
+
+      expect(response).to.include.all.keys('_id', 'name', 'quantity');
+    });
+  });
+});
+
+
+describe('Busca apenas um produto no BD através do nome', () => {
+  const DBServer = new MongoMemoryServer();
+  let connectionMock;
+
+  const NAME_EXAMPLE = 'example';
+
+  before(async () => {
+    const URLMock = await DBServer.getUri();
+    connectionMock = await MongoClient
+     .connect(URLMock, {
+       useNewUrlParser: true,
+       useUnifiedTopology: true
+     })
+     .then((conn) => conn.db('StoreManager'));
+
+    
+    sinon.stub(mongoConnection, 'getConnection').resolves(connectionMock);
+  });
+
+  after(() => {
+    mongoConnection.getConnection.restore();
+  });
+
+  describe('quando não existe um produto para o nome informado', () => {
+    it('retorna "false"', async () => {
+      const response = await productsModel.findProductByName(NAME_EXAMPLE);
+
+      expect(response).to.be.equal(false);
+    });
+  });
+
+  describe('quando existe um produto para o nome informado', () => {
+    const expectedProduct = {
+      _id: ObjectId('604cb554311d68f491ba5781'),
+      name: 'example',
+      quantity: 20,
+    }
+
+    before(async () => {
+      const db = await mongoConnection.getConnection();
+      await db.collection('products').insertOne({ ... expectedProduct });
+    });
+
+    after(async () => {
+      const db = await mongoConnection.getConnection();
+      await db.collection('products').drop();
+  });
+
+    it('retorna true', async () => {
+      const response = await productsModel.findProductByName(NAME_EXAMPLE);
+
+
+      expect(response).to.be.equal(true);
+    });
+  });
+});
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+describe('Atualiza apenas um produto no BD através do id', () => {
+  const DBServer = new MongoMemoryServer();
+  let connectionMock;
+
+  const NAME_EXAMPLE = 'example';
+
+  before(async () => {
+    const URLMock = await DBServer.getUri();
+    connectionMock = await MongoClient
+     .connect(URLMock, {
+       useNewUrlParser: true,
+       useUnifiedTopology: true
+     })
+     .then((conn) => conn.db('StoreManager'));
+
+    
+    sinon.stub(mongoConnection, 'getConnection').resolves(connectionMock);
+  });
+
+  after(() => {
+    mongoConnection.getConnection.restore();
+  });
+
+  describe('quando não existe um produto para o nome informado', () => {
+    it('retorna "false"', async () => {
+      const response = await productsModel.findProductByName(NAME_EXAMPLE);
+
+      expect(response).to.be.equal(false);
+    });
+  });
+
+  describe('quando existe um produto para o nome informado', () => {
+    const expectedProduct = {
+      _id: ObjectId('604cb554311d68f491ba5781'),
+      name: 'example',
+      quantity: 20,
+    }
+
+    before(async () => {
+      const db = await mongoConnection.getConnection();
+      await db.collection('products').insertOne({ ... expectedProduct });
+    });
+
+    after(async () => {
+      const db = await mongoConnection.getConnection();
+      await db.collection('products').drop();
+  });
+
+    it('retorna um objeto', async () => {
+      const expected_response = { 
+        id: '604cb554311d68f491ba5781',
+        name: 'update_test',
+        quantity: 10
+      }
+      const response = await productsModel.update('604cb554311d68f491ba5781', 'update_test', 10);
+ 
+      expect(response).to.be.deep.equal(expected_response);
+    });
+  });
+});
+
+///////////////////////////////////////////////////////////////////////////
+
+
+describe('Atualiza apenas um produto no BD através do id', () => {
+  const DBServer = new MongoMemoryServer();
+  let connectionMock;
+
+  const NAME_EXAMPLE = 'example';
+
+  before(async () => {
+    const URLMock = await DBServer.getUri();
+    connectionMock = await MongoClient
+     .connect(URLMock, {
+       useNewUrlParser: true,
+       useUnifiedTopology: true
+     })
+     .then((conn) => conn.db('StoreManager'));
+
+    
+    sinon.stub(mongoConnection, 'getConnection').resolves(connectionMock);
+  });
+
+  after(() => {
+    mongoConnection.getConnection.restore();
+  });
+
+  describe('quando não existe um produto para o nome informado', () => {
+    it('retorna "false"', async () => {
+      const response = await productsModel.findProductByName(NAME_EXAMPLE);
+
+      expect(response).to.be.equal(false);
+    });
+  });
+
+  describe('Deleta um objeto a partir do Id informado', () => {
+    const expectedProduct = {
+      _id: ObjectId('604cb554311d68f491ba5781'),
+      name: 'example',
+      quantity: 20,
+    }
+
+    before(async () => {
+      const db = await mongoConnection.getConnection();
+      await db.collection('products').insertOne({ ... expectedProduct });
+    });
+
+    after(async () => {
+      const db = await mongoConnection.getConnection();
+      await db.collection('products').drop();
+  });
+
+    it('retorna um objeto', async () => {
+      const response = await productsModel.exclude('604cb554311d68f491ba5781');
+ 
+      expect(response.result.ok).to.be.deep.equal(1);
+    });
   });
 });
